a4dd178f48cc1e4921f1014d8fb2e1be5e81640c
mihajloo <42943109+mihajloo@users.noreply.github.com>
2019-06-18 19:04:24 +0200

    vd

--- js/funkcije.js
+++ js/funkcije.js
@@ -188,7 +188,232 @@ if(localStorage.getItem('restorani') === null){
 							
 							)
 						}
+						),
+				"Lion" :
+					new Array(
+					{
+					"ime" : "Zvezdara Teatar",
+					"imeEng" : "Zvezdara Theatre",
+					"tipHraneEng":"International",
+					"tipHrane" : "Internacionalna",
+					"lokacija" : "Milana Rakića 38",
+					"lokacijaEng":"The Street of Milan Rakić 38",
+					"ocena" : 4.57,
+					"brojGlasova": 1,
+					"stranica" : "zvezdarateatar.html",
+					"stranicaEng" : "zvezdarateatarEng.html",
+					"stranicaDostava":"hranaTeatar.html",
+					"stranicaDostavaEng":"hranaTeatarEng.html",
+					"hrana" : 
+						new Array(
+						{
+							"naziv" : "Francuski doručak",
+							"nazivEng" :"French breakfast",
+							"cena"  : 560,
+							"slika" : "francuskidorucak.jpg"			
+						},
+						{
+							"naziv" : "Piletina u Bechamel sosu",
+							"nazivEng" :"Chicken in Bechamel sauce",
+							"cena"  : 1080,
+							"slika" : "piletinaubechamelsosu.jpg"			
+						},
+						{
+							"naziv" : "Proja",
+							"nazivEng":"Corn Bread",
+							"cena"  : 180,
+							"slika" : "proja.jpg"			
+						}
+						
 						)
+					},
+					{
+					"ime" : "Cuoco",
+					"imeEng" : "Cuoco",
+					"tipHrane" : "Italijanska",
+					"tipHraneEng" : "Italian",
+					"lokacija" : "Bulevar Kralja Aleksandra 250",
+					"lokacijaEng":"King Aleksandar Boulevard 250",					
+					"ocena" : 4.67,
+					"brojGlasova": 1,
+					"stranica" : "cuoco.html",
+					"stranicaEng" : "cuocoEng.html",
+					"stranicaDostava":"hranaCuoco.html",
+					"stranicaDostavaEng":"hranaCuocoEng.html",
+					"hrana" : 
+						new Array(
+						{
+							"naziv" : "Pica Kaprićoza",
+							"nazivEng": "Pizza Capricciosa",
+							"cena"  : 710,
+							"slika" : "pizza-capricciosa.jpg"			
+						},
+						{
+							"naziv" : "Pica Napoli",
+							"nazivEng": "Pizza Napoli",
+							"cena"  : 640,
+							"slika" : "pizza-napoli.jpg"			
+						},
+						{
+							"naziv" : "Pica sa mortadelom",
+							"nazivEng": "Pizza with Mortadela",
+							"cena"  : 770,
+							"slika" : "pizza-mortadela.jpg"			
+						}
+						
+						)
+					},
+					{
+					"ime" : "Klopka",
+					"imeEng" : "Trap",					
+					"tipHrane" : "Domaća",
+					"tipHraneEng" : "Traditional",					
+					"lokacija" : "Batutova 13",
+					"lokacijaEng" : "Batutova 13",					
+					"ocena" : 4.20,
+					"brojGlasova": 1,
+					"stranica" : "klopka.html",
+					"stranicaEng" : "klopkaEng.html",
+					"stranicaDostava":"hranaKlopka.html",
+					"stranicaDostavaEng":"hranaKlopkaEng.html",
+					"hrana" : 
+						new Array(
+						{
+							"naziv" : "Ćevapi",
+							"nazivEng" : "Chevaps",							
+							"cena"  : 560,
+							"slika" : "cevapii.jpg"			
+						},
+						{
+							"naziv" : "Robinja",
+							"nazivEng" : "Slave",							
+							"cena"  : 800,
+							"slika" : "robinja.jpg"			
+						},
+						{
+							"naziv" : "Mešano meso",
+							"nazivEng":"Mixed meat",
+							"cena"  : 1480,
+							"slika" : "mesanomeso.jpg"			
+						}
+						
+						)
+					},
+					
+					),
+
+				"Palilula" :
+					new Array(
+					{
+					"ime" : "Zlatar",
+					"imeEng":"Goldsmith",
+					"tipHrane" : "Domaća",
+					"tipHraneEng":"Traditional",
+					"lokacija" : "Preradovićeva 9a",
+					"lokacijaEng" : "Preradovićeva 9a",
+					"ocena" : 4.80,
+					"brojGlasova": 1,
+					"stranica" : "zlatar.html",
+					"stranicaEng" : "zlatarEng.html",
+					"stranicaDostava":"hranaZlatar.html",
+					"stranicaDostavaEng":"hranaZlatarEng.html",
+					"hrana" : 
+						new Array(
+						{
+							"naziv" : "Pohovani kačkavalj",
+							"nazivEng":"Fried Cheese",
+							"cena"  : 450,
+							"slika" : "pohovani.jpg"			
+						},
+						{
+							"naziv" : "Kačamak smočani",
+							"nazivEng":"Polenta",
+							"cena"  : 1200,
+							"slika" : "kacamak.jpg"			
+						},
+						{
+							"naziv" : "Prebranac",
+							"nazivEng":"Baked Beans",
+							"cena"  : 320,
+							"slika" : "prebranac.jpg"			
+						}
+						
+						)
+					},
+					{
+					"ime" : "Lorenzo & Kakalamba",
+					"imeEng" : "Lorenzo & Kakalamba",
+					"tipHrane" : "Italijanska",
+					"tipHraneEng" : "Italian",
+					"lokacija" : "Cvijićeva 110",
+					"lokacijaEng" : "Cvijićeva 110",
+					"ocena" : 4.90,
+					"brojGlasova": 1,
+					"stranica" : "lorenzo.html",
+					"stranicaEng" : "lorenzoEng.html",
+					"stranicaDostava":"hranaLorenzo.html",
+					"stranicaDostavaEng":"hranaLorenzoEng.html",
+					"hrana" : 
+						new Array(
+						{
+							"naziv" : "Pate od guščije džigerice",
+							"nazivEng":"Pate made of Goose liver",
+							"cena"  : 1400,
+							"slika" : "pate.jpg"			
+						},
+						{
+							"naziv" : "Peglana Kobasica",
+							"nazivEng" : "Roasted Sausage",
+							"cena"  : 1300,
+							"slika" : "peglanakobasica.jpg"			
+						},
+						{
+							"naziv" : "Šušpe",
+							"nazivEng":"Stuffed Peppers",
+							"cena"  : 900,
+							"slika" : "suspe.jpg"			
+						}
+						
+						)
+					},
+					{
+					"ime" : "Mala Kolubara",
+					"imeEng":"Little Kolubara",
+					"tipHrane" : "Domaća",
+					"tipHraneEng":"Traditional",
+					"lokacija" : "Savski nasip 9a",
+					"lokacijaEng" : "Mound of Sava 9a",					
+					"ocena" : 4.70,
+					"brojGlasova": 1,
+					"stranica" : "malakolubara.html",
+					"stranicaEng" : "malakolubaraEng.html",
+					"stranicaDostava":"hranaMalaKolubara.html",
+					"stranicaDostavaEng":"hranaMalaKolubaraEng.html",
+					"hrana" : 
+						new Array(
+						{
+							"naziv" : "Praseće pečenje",
+							"nazivEng":"Roasted pork",
+							"cena"  : 1500,
+							"slika" : "pecenje.jpg"			
+						},
+						{
+							"naziv" : "Punjene Lignje",
+							"nazivEng":"Stuffed Squids",
+							"cena"  : 1700,
+							"slika" : "lignje.jpg"			
+						},
+						{
+							"naziv" : "Teletina ispod sača",
+							"nazivEng":"Beal under the Sac",
+							"cena"  : 1650,
+							"slika" : "teletetinaispodsaca.jpg"			
+						}
+						
+						)
+					},
+					
+					)		
 						
 						};
 				
@@ -203,8 +428,9 @@ function loadBest(){
 	var restorani = JSON.parse(localStorage.getItem('restorani'));
 	var restoraniBest = new Array();
 	restoraniBest = restoraniBest.concat(restorani.Zvezdara);
-	restoraniBest = restoraniBest.concat(restorani.Vračar);
 	restoraniBest = restoraniBest.concat(restorani['Novi Beograd']);
+	restoraniBest = restoraniBest.concat(restorani.Lion);
+	restoraniBest = restoraniBest.concat(restorani.Palilula);
 	restoraniBest.sort(function f(a,b){if(a["ocena"]<b["ocena"]) return 1; else if (a["ocena"]>b["ocena"]) return -1; else return 0;})
 	var str="";
 	for(var i = 0;i<3;i++){
@@ -253,7 +479,7 @@ restorani[opstina].sort(function(a,b){
 });
 for (var i = 0; i < restorani[opstina].length;i++){
 	
-	str+="<div class=\"card mt-3 mb-3\" style=\"width:25rem\" onclick=\"window.location.href=\'"+restorani[opstina][i]["stranicaEng"]+"\'\"><div class=\"card-body\">";
+	str+="<div class=\"card mt-3 mb-3\" style=\"width:25rem\" onclick=\"window.location.href=\'"+restorani[opstina][i]["stranica"]+"\'\"><div class=\"card-body\">";
 	str+="<div class=\"card-text\"><b>Naziv restorana: </b>" + restorani[opstina][i].ime +"</div>";
 	str+="<div class=\"card-text\"><b>Tip hrane: </b>" + restorani[opstina][i].tipHrane +"</div>"; 
 	str+="<div class=\"card-text\"><b>Lokacija: </b>" + restorani[opstina][i].lokacija +"</div>"; 
@@ -266,8 +492,9 @@ function ucitajDostava(){
 	var restorani = JSON.parse(localStorage.getItem('restorani'));
 	var restoraniBest = new Array();
 	restoraniBest = restoraniBest.concat(restorani.Zvezdara);
-	//restoraniBest = restoraniBest.concat(restorani.Vračar);
 	restoraniBest = restoraniBest.concat(restorani['Novi Beograd']);
+	restoraniBest = restoraniBest.concat(restorani.Lion);
+	restoraniBest = restoraniBest.concat(restorani.Palilula);
 	var str="";
 	for(var i = 0;i<restoraniBest.length;i++){
 	str+="<div class=\"card mt-3 mb-3\" style=\"width:25rem\" onclick=\"window.location.href=\'"+restoraniBest[i]["stranicaDostava"]+"\'\"><div class=\"card-body\">";
@@ -282,8 +509,9 @@ function filtrirajPoTipuHrane(){
 	var restorani = JSON.parse(localStorage.getItem('restorani'));
 	var restoraniBest = new Array();
 	restoraniBest = restoraniBest.concat(restorani.Zvezdara);
-	//restoraniBest = restoraniBest.concat(restorani.Vračar);
 	restoraniBest = restoraniBest.concat(restorani['Novi Beograd']);
+	restoraniBest = restoraniBest.concat(restorani.Lion);
+	restoraniBest = restoraniBest.concat(restorani.Palilula);
 	var str="";
 	for(var i = 0;i<restoraniBest.length;i++){
 	if(!document.getElementById(restoraniBest[i]["tipHrane"]).checked) continue;	
@@ -304,7 +532,7 @@ function ucitajHranu(opstina,restoran){
 	str+="<div class=\"card mt-3\"><div class=\"row no-gutters\"><div class=\"col-auto\"> <img src=\"images/"+hrana[i].slika + "\"  width=200 height=200></div>";
 	str+="<div class=\"col\"><div class=\"card-block px-2\"><p class=\"card-text\"><b>Naziv jela: </b>" +hrana[i].naziv +"</p>";
 	str+="<p class=\"card-text\"><b>Cena: </b>" +hrana[i].cena +" dinara</p>";
-	str+="<select class='form-control' id='"+hrana[i].naziv+"'><option value='0'>Izaberite količinu...</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>"
+	str+="<input  type='number' class='form-control' id='"+hrana[i].naziv+"' placeholder='Izaberite količinu hrane...' min=0>"
 	str+="</div></div></div></div>"
 	}
 	str+="<div class='row mt-2'><div class='col text-center'><button type='button' onclick=\"naruciHranu('"+opstina+"','"+restoran+"')\" class='btn btn-outline-primary'>Naručite</button></div></div>"
@@ -407,8 +635,9 @@ function loadBestEng(){
 	var restorani = JSON.parse(localStorage.getItem('restorani'));
 	var restoraniBest = new Array();
 	restoraniBest = restoraniBest.concat(restorani.Zvezdara);
-	restoraniBest = restoraniBest.concat(restorani.Vračar);
 	restoraniBest = restoraniBest.concat(restorani['Novi Beograd']);
+	restoraniBest = restoraniBest.concat(restorani.Lion);
+	restoraniBest = restoraniBest.concat(restorani.Palilula);
 	restoraniBest.sort(function f(a,b){if(a["ocena"]<b["ocena"]) return 1; else if (a["ocena"]>b["ocena"]) return -1; else return 0;})
 	var str="";
 	for(var i = 0;i<3;i++){
@@ -470,8 +699,9 @@ function ucitajDostavaEng(){
 	var restorani = JSON.parse(localStorage.getItem('restorani'));
 	var restoraniBest = new Array();
 	restoraniBest = restoraniBest.concat(restorani.Zvezdara);
-	//restoraniBest = restoraniBest.concat(restorani.Vračar);
 	restoraniBest = restoraniBest.concat(restorani['Novi Beograd']);
+	restoraniBest = restoraniBest.concat(restorani.Lion);
+	restoraniBest = restoraniBest.concat(restorani.Palilula);
 	var str="";
 	for(var i = 0;i<restoraniBest.length;i++){
 	str+="<div class=\"card mt-3 mb-3\" style=\"width:25rem\" onclick=\"window.location.href=\'"+restoraniBest[i]["stranicaDostavaEng"]+"\'\"><div class=\"card-body\">";
@@ -486,8 +716,9 @@ function filtrirajPoTipuHraneEng(){
 	var restorani = JSON.parse(localStorage.getItem('restorani'));
 	var restoraniBest = new Array();
 	restoraniBest = restoraniBest.concat(restorani.Zvezdara);
-	//restoraniBest = restoraniBest.concat(restorani.Vračar);
 	restoraniBest = restoraniBest.concat(restorani['Novi Beograd']);
+	restoraniBest = restoraniBest.concat(restorani.Lion);
+	restoraniBest = restoraniBest.concat(restorani.Palilula);
 	var str="";
 	for(var i = 0;i<restoraniBest.length;i++){
 	if(!document.getElementById(restoraniBest[i]["tipHrane"]).checked) continue;	
@@ -508,7 +739,7 @@ function ucitajHranuEng(opstina,restoran){
 	str+="<div class=\"card mt-3\"><div class=\"row no-gutters\"><div class=\"col-auto\"> <img src=\"images/"+hrana[i].slika + "\"  width=200 height=200></div>";
 	str+="<div class=\"col\"><div class=\"card-block px-2\"><p class=\"card-text\"><b>Name of Food: </b>" +hrana[i].nazivEng +"</p>";
 	str+="<p class=\"card-text\"><b>Price: </b>" +hrana[i].cena +" dinars</p>";
-	str+="<select class='form-control' id='"+hrana[i].naziv+"'><option value='0'>Choose the ammount of food...</option><option value='1'>1</option><option value='2'>2</option><option value='3'>3</option></select>"
+	str+="<input  type='number' class='form-control' id='"+hrana[i].naziv+"' placeholder='Choose the ammount of food...' min=0>"
 	str+="</div></div></div></div>"
 	}
 	str+="<div class='row mt-2'><div class='col text-center'><button type='button' onclick=\"naruciHranuEng('"+opstina+"','"+restoran+"')\" class='btn btn-outline-primary'>Order</button></div></div>"
@@ -596,7 +827,7 @@ function loadNarudzbineEng(){
 	str+="<div class=\"card-text mt-1\"><button type='button' onclick=\"savePDFEng('"+narudzbine[i].nazivEng+"',"+narudzbine[i].cena+",'"+narudzbine[i].datum+"')\"class='btn btn-primary'>Save as PDF</button></div></div></div>";
 	}
 	}
-	else {str = "<div class=\"mt-3\" style=\"text-align:center;\">Nemate narudžbina za sada!</div>"}
+	else {str = "<div class=\"mt-3\" style=\"text-align:center;\">You don't have any orders currently!</div>"}
 	document.getElementById("narudzbine").innerHTML = str;
 }
 
